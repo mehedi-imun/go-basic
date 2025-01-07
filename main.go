531973f600156d7f6c227bb026ee52d33d95f1a5
mehedi imun <mehediimun@gmail.com>
2025-01-07 14:20:18 +0600

    hello go

---
+++ main.go
@@ -0,0 +1,16 @@
+package main 
+import "fmt"
+func main (){
+	fmt.Println("hello world!")
+
+	// variable 
+	age:=20
+
+	// if else
+	if age > 17{
+		fmt.Println("you can married")
+	} else{
+		fmt.Println("can't")
+	}
+
+}  
