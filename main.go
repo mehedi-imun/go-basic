76f9e6ccd75fd4899e2a82a01cc159697c04cc77
mehedi imun <mehediimun@gmail.com>
2025-01-08 16:01:40 +0600

    learn function

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
-package main 
+package main
+
 import "fmt"
+func concat(s1 string, s2 string) string {
+	return s1 + s2
+}
 func main (){
 	fmt.Println("hello world!")
 
@@ -13,4 +17,13 @@ func main (){
 		fmt.Println("can't")
 	}
 
+	test("Lane,", " happy birthday!")
+	test("Elon,", " hope that Tesla thing works out")
+	test("Go", " is fantastic")
+
 }  
+
+
+func test(s1 string, s2 string) {
+	fmt.Println(concat(s1, s2))
+}
